ae350e22529233c381dee877708a235bd20e1b25
Robbys <robbys@Robbyss-MacBook-Pro.local>
2025-07-01 23:51:55 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+/* Menu show */
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+/* Menu hidden */
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+// tutup menu jika di scroll
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    navMenu.classList.remove("show-menu");
+  }
+});
+
+// tutup menu jika klik di luar nav__menu
+document.addEventListener("click", (e) => {
+  const isClickInside =
+    navMenu.contains(e.target) || navToggle.contains(e.target);
+
+  if (!isClickInside) {
+    navMenu.classList.remove("show-menu");
+  }
+});
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll(".nav__link");
+
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
+};
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/*=============== ADD SHADOW HEADER ===============*/
+const shadowHeader = () => {
+  const header = document.getElementById("header");
+  // Add a class if the bottom offset is greater than 50 of the viewport
+  this.scrollY >= 50
+    ? header.classList.add("shadow-header")
+    : header.classList.remove("shadow-header");
+};
+window.addEventListener("scroll", shadowHeader);
+
